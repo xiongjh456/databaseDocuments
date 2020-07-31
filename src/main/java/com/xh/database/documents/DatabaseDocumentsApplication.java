1b441c26c534b830c29c94924bb6492785bdff86
xiongxiongj <169534@agih>
2020-07-31 09:56:14 +0800

    项目初始化

---
+++ src/main/java/com/xh/database/documents/DatabaseDocumentsApplication.java
@@ -0,0 +1,13 @@
+package com.xh.database.documents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseDocumentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseDocumentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xh/database/documents/DatabaseDocumentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xh.database.documents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseDocumentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
